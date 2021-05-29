2d19cb06e54b4246ac7dfbb6596bd360acf436a8
anthony.chassier@outlook.fr <anthony.chassier@outlook.fr>
2021-05-29 17:55:51 +0200

    Updates

---
+++ precache-manifest.07648783105bc2e5be82e4caae31837e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46592812b4212fd4a6e9ba17c155da4d",
+    "url": "/SimpleStorage2/index.html"
+  },
+  {
+    "revision": "8fcc66a89aaee7feefc1",
+    "url": "/SimpleStorage2/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ada8436b4ad1b0c85c25",
+    "url": "/SimpleStorage2/static/js/2.28b7707d.chunk.js"
+  },
+  {
+    "revision": "8fcc66a89aaee7feefc1",
+    "url": "/SimpleStorage2/static/js/main.b91107f6.chunk.js"
+  },
+  {
+    "revision": "139f5c39cefa87953860",
+    "url": "/SimpleStorage2/static/js/runtime-main.60165aaa.js"
+  }
+]);
